4cd72ec887f2443f84a6f891ab6205417a8d3b47
allykeightley <allysonkeightley@outlook.com>
2025-04-27 16:18:34 -0700

    Need to add bonus feature

--- index.js
+++ index.js
@@ -154,13 +154,9 @@ const unfunded = GAMES_JSON.filter( (game) => {
     return game.goal > game.pledged;
 }).length;
 
-const funded = GAMES_JSON.filter( (game) => {
-    return game.goal <= game.pledged;
-}).length;
-
 // create a string that explains the number of unfunded games using the ternary operator
-const unfundedGamesPlural = `A total of $${totalRaised.toLocaleString('en-US')} has been raised for ${funded} games. Currently, ${unfunded} games remain unfunded. We need your help to fun these amazing games!`
-const unfundedGamesSingular = `A total of $${totalRaised.toLocaleString('en-US')} has been raised for ${funded} game. Currently, ${unfunded} game remains unfunded. We need your help to fun these amazing games!`
+const unfundedGamesPlural = `A total of $${totalRaised.toLocaleString('en-US')} has been raised for ${totalGames} games. Currently, ${unfunded} games remain unfunded. We need your help to fun these amazing games!`
+const unfundedGamesSingular = `A total of $${totalRaised.toLocaleString('en-US')} has been raised for ${totalGames} game. Currently, ${unfunded} game remains unfunded. We need your help to fun these amazing games!`
 
 const fundingString = unfunded > 1 ? unfundedGamesPlural : unfundedGamesSingular;
 // create a new DOM element containing the template string and append it to the description container
@@ -181,7 +177,14 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame, ...otherGames ] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topPledgeGame = document.createElement("p");
+topPledgeGame.textContent = firstGame.name;
+firstGameContainer.appendChild(topPledgeGame);
 
 // do the same for the runner up item
+const runnerUpGame = document.createElement("p");
+runnerUpGame.textContent = secondGame.name;
+secondGameContainer.appendChild(runnerUpGame);
